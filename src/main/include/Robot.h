d32166170fce7847a1690771004acaf507d4dd9d
HugoHead <deanmartin@830>
2024-04-14 15:05:17 -0400

    copy swervebase files to commandbase training except for controller interface

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -4,31 +4,62 @@
 
 #pragma once
 
-#include <optional>
-
+#include <string>
 #include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include "AnalogAbsoluteEncoder.h"
+#include "NeoTurnMotor.h"
+#include "NeoDriveMotor.h"
+#include "WPISwerveModule.h"
+#include "WPISwerveDrive.h"
+#include "NavXGyro.h"
+#include <array>
+#include <frc/XboxController.h>
 #include <frc2/command/CommandPtr.h>
-
-#include "RobotContainer.h"
+#include <memory>
+#include "RobotControlData.h"
+#include <frc2/command/CommandPtr.h>
+#include <filesystem>
+#include <frc/Filesystem.h>
 
 class Robot : public frc::TimedRobot {
  public:
   void RobotInit() override;
   void RobotPeriodic() override;
-  void DisabledInit() override;
-  void DisabledPeriodic() override;
   void AutonomousInit() override;
   void AutonomousPeriodic() override;
   void TeleopInit() override;
   void TeleopPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void TestInit() override;
   void TestPeriodic() override;
   void SimulationInit() override;
   void SimulationPeriodic() override;
+  void updateDashBoardValues(); 
+  void SwerveInit();
+  void PrintSwerveInfo();
 
  private:
-  // Have it empty by default so that if testing teleop it
-  // doesn't have undefined behavior and potentially crash.
-  std::optional<frc2::CommandPtr> m_autonomousCommand;
+  static const int NUM_MODULES = 4;
 
-  RobotContainer m_container;
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+
+  std::array<AnalogAbsoluteEncoder, NUM_MODULES> _abs_encoders;
+  std::array<NeoTurnMotor, NUM_MODULES> _turn_motors;
+  std::array<NeoDriveMotor, NUM_MODULES> _drive_motors;
+  std::array<WPISwerveModule, NUM_MODULES> _modules;
+  WPISwerveDrive _swerve;
+
+  NavXGyro _gyro;
+  RobotControlData _robot_control_data;
+
+  int m_state;
+  std::unique_ptr<frc2::CommandPtr> m_auto;
+  std::filesystem::path m_autos_directory;
+  frc::SendableChooser<std::string> m_auto_chooser;
+
 };
